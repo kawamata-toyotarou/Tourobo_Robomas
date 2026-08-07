8791a5a9db4a8eae76637d09782887233f530f7e
toydayo <kawamataro1209@gmail.com>
2026-08-07 19:18:46 +0900

    受信コールバック関数の追加

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -18,6 +18,9 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include "stm32g4xx.h"
+#include "stm32g4xx_hal_def.h"
+#include "stm32g4xx_hal_fdcan.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
@@ -49,6 +52,9 @@ UART_HandleTypeDef huart2;
 
 /* USER CODE BEGIN PV */
 
+FDCAN_HandleTypeDef TxHeader;
+FDCAN_HandleTypeDef RxHeader;
+
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -65,6 +71,18 @@ static void MX_USART2_UART_Init(void);
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
 
+void HAL_FDCAN_RxFifo1Callback(FDCAN_HandleTypeDef *hfdcan, uint32_t RxFifo1ITs) {
+
+  if ((RxFifo1ITs & FDCAN_IT_RX_FIFO1_NEW_MESSAGE) != RESET) {
+
+    uint8_t RxData[12];
+
+    if (HAL_FDCAN_GetRxMessage(&hfdcan3, FDCAN_RX_FIFO1, &RxHeader, RxData) != HAL_OK) {
+      Error_Handler();
+    }
+
+  }
+}
 /* USER CODE END 0 */
 
 /**
