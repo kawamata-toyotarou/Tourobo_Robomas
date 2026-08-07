e613b758ae4754d4c180ce16388a9f4e74009261
toydayo <kawamataro1209@gmail.com>
2026-08-07 20:37:36 +0900

    write関数を追加したprintfを使えるようにした

--- Core/Inc/can_communication_function.h
+++ Core/Inc/can_communication_function.h
@@ -1,4 +1,3 @@
-/*受信コールバック関数*/
 #ifndef CAN_COMMUNICATION_FUNCTION_H
 #define CAN_COMMUNICATION_FUNCTION_H
  
@@ -9,7 +8,8 @@ extern FDCAN_HandleTypeDef hfdcan1;
 extern FDCAN_HandleTypeDef hfdcan3;
 extern FDCAN_TxHeaderTypeDef TxHeader;
 extern FDCAN_RxHeaderTypeDef RxHeader;
- 
+
+/*受信コールバック関数*/
 void HAL_FDCAN_RxFifo1Callback(FDCAN_HandleTypeDef *hfdcan, uint32_t RxFifo1ITs);
  
 #endif /* CAN_COMMUNICATION_FUNCTION_H */
--- Core/Src/main.c
+++ Core/Src/main.c
@@ -66,6 +66,12 @@ static void MX_USART2_UART_Init(void);
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
 
+int write(int file, char *ptr, int len) {
+
+  HAL_UART_Transmit(&huart2, (uint8_t*)ptr, len, 10);  //printfを使えるにした
+  return len;
+
+}
 
 /* USER CODE END 0 */
 
